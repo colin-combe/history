4bdac401e48149f1412ad47cfb3d366309bcd4e7
colin-combe <colin.combe@ed.ac.uk>
2018-06-14 11:34:31 +0100

    wip

--- php/xiUI_uploads.php
+++ php/xiUI_uploads.php
@@ -1,14 +1,14 @@
 <?php
     session_start();
-    include('./utils.php');
-    //you could comment out the following line and have no login authentication. 
+    include('../../vendor/php/utils.php');
+    //you could comment out the following line and have no login authentication.
     //ajaxBootOut();
 
     include('../../connectionString.php');
     //open connection
     try {
         // @ suppresses non-connection throwing an uncatchable error, so we can generate our own error to catch
-        $dbconn = @pg_connect($connectionString);    //or die('Could not connect: ' . pg_last_error());
+        $dbconn = pg_connect($connectionString) or die('Could not connect: ' . pg_last_error());
 
         if ($dbconn) {
             //error_log (print_r ($_SESSION, true));
@@ -16,28 +16,28 @@
             //~ $searches = $_POST["searches"];
             $userRights["canAddNewSearch"] = true;//getUserRights ($dbconn, $_SESSION['user_id']);
             $userRights["doesUserGUIExist"] = true;//getUserRights ($dbconn, $_SESSION['user_id']);
-            
+
             /*
             $qPart1 = "SELECT id, notes, user_name, submit_date, name, status, random_id, hidden, file_name, enzyme, crosslinkers from
 
-            (select search.id, search.is_executing, search.hidden, search.notes, user_name as user_name, search.submit_date AS submit_date, 
-            search.name AS name, search.status AS status, search.random_id AS random_id, 
+            (select search.id, search.is_executing, search.hidden, search.notes, user_name as user_name, search.submit_date AS submit_date,
+            search.name AS name, search.status AS status, search.random_id AS random_id,
             string_agg(sequence_file.file_name,',') AS file_name
             FROM search
             INNER JOIN users on search.uploadedby = users.id
-            
+
             INNER JOIN search_sequencedb on search.id = search_sequencedb.search_id
-            INNER JOIN sequence_file on search_sequencedb.seqdb_id = sequence_file.id 
+            INNER JOIN sequence_file on search_sequencedb.seqdb_id = sequence_file.id
              "
             ;
 
             // if can_see_all but not a superuser insert this clause
             $canSeeOthersPublic = "WHERE ((COALESCE (search.private, FALSE) = FALSE AND COALESCE (users.hidden, FALSE) = FALSE) OR search.uploadedby = $1) ";
-            
+
             $canSeeMineOnly = "WHERE search.uploadedby = $1 ";
             $canSeeMineOnlyIJ = "WHERE search.uploadedby = $1 ";
             $innerJoinMine = ($searches == "MINE" ? $canSeeMineOnlyIJ : "");
-            
+
             $hideHiddenSearches = " AND COALESCE (search.hidden, FALSE) = FALSE ";
 
             $qPart3 = "
@@ -60,13 +60,13 @@
 
             ORDER BY (CASE WHEN status = 'queuing' THEN 0 WHEN is_executing THEN 1 ELSE 2 END) ASC, srch.id DESC ;
             ";*/
-            
+
             /*
             $qPart1 = "SELECT search.id, search.notes, user_name as user_name, search.submit_date AS submit_date, search.name AS name, search.status AS status, search.random_id AS random_id, sequence_file.file_name AS file_name, enzyme.name AS enzyme
                 FROM search
                 INNER JOIN users on search.uploadedby = users.id
                 INNER JOIN search_sequencedb on search.id = search_sequencedb.search_id
-                INNER JOIN sequence_file on search_sequencedb.seqdb_id = sequence_file.id 
+                INNER JOIN sequence_file on search_sequencedb.seqdb_id = sequence_file.id
                 INNER JOIN parameter_set on search.paramset_id = parameter_set.id
                 INNER JOIN enzyme on parameter_set.enzyme_chosen = enzyme.id
                 WHERE "
@@ -82,14 +82,14 @@
             ;
             */
 
-            $qPart1 = "select origin, filename, error_type, upload_error, upload_warnings, spectra_formats from uploads ORDER by origin;"
+            $qPart1 = "select * from uploads;"
             ;
 
 
             //~ if (!$userRights["isSuperUser"] || $searches == "MINE") {
                 //~ $privateClause = ($searches == "MINE" ? $canSeeMineOnly : $canSeeOthersPublic).(!$userRights["isSuperUser"] ? $hideHiddenSearches : "");
                 pg_prepare($dbconn, "my_query", $qPart1);
-                $result = pg_execute($dbconn, "my_query");
+                $result = pg_execute($dbconn, "my_query");//, [$_SESSION['user_id']]);
             //~ } else {
                 //~ pg_prepare($dbconn, "my_query", $qPart1.$qPart3);
                 //~ $result = pg_execute($dbconn, "my_query", []);
